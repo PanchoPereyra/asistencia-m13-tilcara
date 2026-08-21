0858f2117d5b7e94f604bba1f7148177197a2467
Pancho_Pereyra <panchopereyra@gmail.com>
2026-08-21 06:31:08 -0300

    modificando para actualice los cambios

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,10 @@
-const CACHE_NAME = "asistencia-tilcara-v1";
-const urlsToCache = ["/", "/dashboard", "/asistencia", "/jugadores"];
+const CACHE_NAME = "tilcara-cache-v1";
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
   );
 });
 
@@ -23,6 +16,47 @@ self.addEventListener("activate", (event) => {
           .filter((name) => name !== CACHE_NAME)
           .map((name) => caches.delete(name))
       );
-    })
+    }).then(() => self.clients.claim())
   );
 });
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
+
+
+
+// const CACHE_NAME = "asistencia-tilcara-v1";
+// const urlsToCache = ["/", "/dashboard", "/asistencia", "/jugadores"];
+
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+// self.addEventListener("activate", (event) => {
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames
+//           .filter((name) => name !== CACHE_NAME)
+//           .map((name) => caches.delete(name))
+//       );
+//     })
+//   );
+// });
